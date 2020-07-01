e1623a7afd5b2fc4964b2c4fc8996430b0a56385
Chloe Steffens <caforonda103@yahoo.com>
2020-07-01 03:00:20 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,6 @@ class ApplicationController < Sinatra::Base
 
 	post "/signup" do
 		user = User.new(:username => params[:username], :password => params[:password])
-		
 		if user.save
       redirect "/login"
     else
@@ -32,7 +31,6 @@ class ApplicationController < Sinatra::Base
 
 	post "/login" do
 		user = User.find_by(:username => params[:username])
-		
 		if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect "/success"
